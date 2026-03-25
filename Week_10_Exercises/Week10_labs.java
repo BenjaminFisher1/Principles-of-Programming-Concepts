982e19aaf868d1a033f0543cdfa53a82169278e6
Ben Fisher <BenLfish00@gmail.com>
2026-03-25 14:20:20 -0400

    Mv in week10 boilerplate

---
+++ Week_10_Exercises/Week10_labs.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.*;
+import java.util.function.*;
+import java.util.Map;
+
+/** The goal of this practice is not to "get the answer" through AI or some other means, but for you to think through the questions and
+* come up with a strategy. You can decide not to do it at your own cost.
+*/
+
+/**
+* In the following, write code to achieve what's asked. You don't need to but if you want to very the accuracy of your code,
+* include statements to print the result.
+*/
+
+/** Submit to the TA, and he will assign you a grade based on a few selected responses.  */
+
+
+
+
+
+public class Week10_labs
+{
+    public static void main(String[] args)
+	{
+        List<String> fruit = Arrays.asList("cherry","banana","berry","apple","cherry","kiwi","fig","date","lemon","honeydew","cherry","elderberry","apple","banana","grape");
+
+		// Collect elements into a Set
+
+        // Collect the fruit into groups based on their first character
+
+		// Group fruit by the length of the name
+
+		//Collect the fruit that has erry in it
+
+		//Create a partition of fruit based on if it contains erry
+
+		//collect/ the fruit that has 5 or less symbols
+
+		//find the total number of symbols in all the fruit stored
+
+
+		List<Integer> data = Arrays.asList(87, 23, 45, 100, 6, 78, 92, 44, 13, 56, 34, 99, 82, 19, 1012, 78, 45, 90, 23, 56, 78, 100, 3, 43, 67, 89, 21, 34, 10);
+
+        // Partition data based on if >=50
+
+		//divide data into groups based on the remainder when divided by 7
+
+
+		//find the sum of the data
+
+
+		//collect the unique values
+
+        //compute the cube of each values
+
+		//find the sum of the cubes of each value
+
+		//increase the value of each element by 5
+
+		//compute the cube of the even values
+
+   }
+}
